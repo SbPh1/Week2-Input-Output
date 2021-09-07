99155beb63efa29c47531a980f78b03c3de190b0
Opilane TTHK <opilane@tthk.ee>
2021-09-07 14:18:17 +0300

    readline

---
+++ GreetingV2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreetingV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja käest nime
+            //programm küsib kasutaja käest perekonanime
+            //programm tervitab kasutajat, kasutades neid andmeid
+            Console.WriteLine("Mis on sinu eesnimi?");
+            String FirstName = Console.ReadLine();
+
+            Console.WriteLine("Mis on sinu perekonnanimi?");
+            String LastName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {FirstName} {LastName}");
+
+            // Console.WriteLine("Hello, " + FirstName + " " + LastName);
+
+            Console.Read();
+        }
+    }
+}
